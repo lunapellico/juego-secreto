8143092849ed4fb69c4f2fa5ebf0652d28c3d813
lunapellico <138258603+lunapellico@users.noreply.github.com>
2024-03-07 15:08:18 -0300

    Versión Final Juego número secreto

---
+++ app.js
@@ -0,0 +1,194 @@
+/* Una funcion es un encapsulamiento de la accion que queremos que haga. 
+Entonces en el html en lugar de escribir todo el codigo de javascipt solo llamamos la function. 
+*/
+
+let numeroSecreto = 0;
+let intentos = 0;
+let listaNumerosSorteados = [];
+let numeroMaximo = 10;
+
+
+console.log(numeroSecreto);
+
+function asignarTextoElemento(elemento, texto) {
+    let elementoHtml = document.querySelector(elemento);
+    elementoHtml.innerHTML = texto;
+    return;
+
+}
+
+function verificarIntento() {
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+   
+    //console.log(intentos);
+
+    // El triple igual compara teniendo en cuenta tanto el valor como el tipo de dato
+
+    if (numeroDeUsuario === numeroSecreto) {
+        asignarTextoElemento('p',`Acertaste el número en ${intentos} ${(intentos === 1) ? 'vez' : 'veces'}.`);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    
+    } else {
+        //El usuario no acertó.
+        if (numeroDeUsuario > numeroSecreto) {
+            asignarTextoElemento ('p','El número secreto es menor.');
+        } else {
+            asignarTextoElemento ('p','El número secreto es mayor.');
+        }
+
+        intentos++;
+        limpiarCaja();
+
+    }
+    return;
+}
+
+function limpiarCaja() {
+     document.querySelector('#valorUsuario').value = '';
+   
+}
+
+function generarNumeroSecreto() {
+    let numeroGenerado =  Math.floor(Math.random()*numeroMaximo)+1;
+
+    console.log(numeroGenerado);
+    console.log(listaNumerosSorteados);
+    //Si ya sorteamos todos los números
+    if (listaNumerosSorteados.length == numeroMaximo) {
+        asignarTextoElemento('p','Ya se sortearon todos los números posibles.');
+
+    } else {
+
+     //Si el número generado esta inlcuido en la lista
+
+     if (listaNumerosSorteados.includes(numeroGenerado)) {
+        return generarNumeroSecreto();
+     } else {
+         listaNumerosSorteados.push(numeroGenerado);
+        return numeroGenerado;
+
+     }
+
+    }
+
+}
+
+
+function condicionesIniciales() {
+    asignarTextoElemento('h1','Juego del número secreto');
+    asignarTextoElemento('p',`Indica un número del 1 al ${numeroMaximo}`);
+    numeroSecreto = generarNumeroSecreto();
+    intentos = 1;
+
+}
+
+
+function reiniciarJuego() {
+    //Limpiar la caja 
+    limpiarCaja();
+    //Indicar mensaje de intervalo de números
+    //Generar el número aleatorio
+    //Inicializar el número intentos 
+    condicionesIniciales();
+    //Deshabilitar el boton de nuevo juego
+    document.querySelector('#reiniciar').setAttribute('disabled','true');
+
+}
+
+condicionesIniciales();
+
+
+
+
+
+
+
+/*
+
+//Crear una función que muestre "¡Hola, mundo!" en la consola.
+
+function mostrarHola() {
+    console.log("¡Hola, mundo!");
+  }
+  
+  mostrarHola();
+
+//Crear una función que reciba un nombre como parámetro y muestre "¡Hola, [nombre]!" en la consola.
+
+function mostrarHolaNombre(nombre) {
+    console.log(`¡Hola, ${nombre}!`);
+  }
+  
+  mostrarHolaNombre("Alice");
+
+//Crear una función que reciba un número como parámetro y devuelva el doble de ese número.
+
+function calcularDoble(numero) {
+    return numero * 2;
+  }
+  
+  let resultadoDoble = calcularDoble(5);
+  console.log(resultadoDoble);
+
+//Crear una función que reciba tres números como parámetros y devuelva su promedio.
+
+function calcularPromedio(a, b, c) {
+    return (a + b + c) / 3;
+  }
+  
+  let promedio = calcularPromedio(4, 7, 10);
+  console.log(promedio);
+
+//Crear una función que reciba dos números como parámetros y devuelva el mayor de ellos.
+
+function encontrarMayor(a, b) {
+    return a > b ? a : b;
+  }
+  
+  let numeroMayor = encontrarMayor(15, 9);
+  console.log(numeroMayor);
+
+//Crear una función que reciba un número como parámetro y devuelva el resultado de multiplicar ese número por sí mismo.
+
+function cuadrado(numero) {
+    return numero * numero;
+  }
+  
+  let resultado = cuadrado(2);
+  console.log(resultado);
+  */
+
+
+
+
+  //Crea una función que calcule el índice de masa corporal (IMC) de una persona a partir de su altura en metros y peso en kilogramos, que se recibirán como parámetros.
+
+//Crea una función que calcule el valor del factorial de un número pasado como parámetro.
+
+
+/*Crea una función que convierta un valor en dólares, pasado como parámetro, 
+ y devuelva el valor equivalente en reales(moneda brasileña,si deseas puedes hacerlo con el valor del dólar en tu país).
+ Para esto, considera la cotización del dólar igual a R$4,80.*/
+
+
+//Crea una función que muestre en pantalla el área y el perímetro de una sala rectangular, utilizando la altura y la anchura que se proporcionarán como parámetros.
+
+
+//Crea una función que muestre en pantalla el área y el perímetro de una sala circular, utilizando su radio que se proporcionará como parámetro. Considera Pi = 3,14.
+
+
+
+//Crea una función que muestre en pantalla la tabla de multiplicar de un número dado como parámetro.
+
+//Crea una lista vacía llamada "listaGenerica".
+//Crea una lista de lenguajes de programación llamada "lenguagesDeProgramacion con los siguientes elementos: 'JavaScript', 'C', 'C++', 'Kotlin' y 'Python'.
+//Agrega a la lista "lenguagesDeProgramacion los siguientes elementos: 'Java', 'Ruby' y 'GoLang'.
+//Crea una función que muestre en la consola todos los elementos de la lista "lenguagesDeProgramacion.
+//Crea una función que muestre en la consola todos los elementos de la lista "lenguagesDeProgramacion en orden inverso.
+//Crea una función que calcule el promedio de los elementos en una lista de números.
+//Crea una función que muestre en la consola el número más grande y el número más pequeño en una lista.
+//Crea una función que devuelva la suma de todos los elementos en una lista.
+//Crea una función que devuelva la posición en la lista donde se encuentra un elemento pasado como parámetro, o -1 si no existe en la lista.
+//Crea una función que reciba dos listas de números del mismo tamaño y devuelva una nueva lista con la suma de los elementos uno a uno.
+//Crea una función que reciba una lista de números y devuelva una nueva lista con el cuadrado de cada número.
+
